db74d6ff2d7c8a310f359cc720ce747b4155076f
LaTaevia <lataevia.berry@gmail.com>
2016-04-01 10:35:24 -0700

    update html and js to add working for statement and list items.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
   $("form#pingPongInput").submit(function(event){
+    event.preventDefault();
     var userInput = parseInt($("input#userInput").val());
+    var listItems = "";
+
+    for (index = 1; index <= userInput; index++) {
+      listItems += "<li>";
+      if ((index % 3 === 0) && (index % 5 === 0)) {
+          listItems += "ping-pong";
+      }
+      else if (index % 3 === 0) {
+          listItems += "ping";
+      }
+      else if (index % 5 === 0) {
+          listItems += "pong";
+      }
+      else {
+          listItems += index;
+      }
+      listItems += "</li>";
+      $("#output").empty().append(listItems);
+    }
   });
 });
