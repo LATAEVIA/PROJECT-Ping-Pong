87fe6d0c9e7e66fd648ba748fdee92515c2268f2
LaTaevia <lataevia.berry@gmail.com>
2016-04-01 16:50:10 -0700

    add completed and working project with readme file

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# _Ping Pong Game_
+
+#### _When opened in a browser, will display a counting game displaying ping for multiples of 3, pong for multiples of 5, and pingpong for multiples of 15, 04/01/2016_
+
+#### By _**LaTaevia**_
+
+## Description
+
+_This a static webpage, a snapshot of sorts, assigned as an end-of-week project for my third week at Epicodus. It uses HTML, CSS, Bootstrap, JavaScript and jQuery, and functions as a counting game displaying ping for multiples of 3, pong for multiples of 5, and pingpong for multiples of 15_
+
+## Setup/Installation Requirements
+
+* _Clone through Terminal using git clone https://github.com/LATAEVIA/pingPong.git_
+* _Project Files will be in your Downloads folder_
+* _Open files in any text editor to view source code_
+* _Open index.html in any browser to enjoy_
+* _One could also view this portfolio at http://lataevia.github.io/pingPong/
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _Bootstrap_
+* _JavaScript_
+* _jQuery_
+
+### License
+
+*This software is licensed under the MIT license*
+
+Copyright (c) 2016 **_LaTaevia_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
+// Business Logic
+function pingPongFunct(pingPongBot){
+  var pingPongResults = [];
+  for (index = 1; index <= pingPongBot; index++) {
+    if ((index % 3 === 0) && (index % 5 === 0)) {
+        pingPongResults.push('<li>pingpong</li>');
+    }
+    else if (index % 3 === 0) {
+        pingPongResults.push("<li>ping</li>");
+    }
+    else if (index % 5 === 0) {
+        pingPongResults.push("<li>pong</li>");
+    }
+    else {
+        pingPongResults.push("<li>" + index + "</li>");
+    }
+  }return pingPongResults;
+};
+
+// User Interface Logic
+$(document).ready(function() {
+  $("form#pingPongInput").submit(function(event){
+    event.preventDefault();
+    var input = parseInt($("input#userInput").val());
+    var results = pingPongFunct(input);
+    $("#output").empty().append(results);
+  });
+});
+
+
 // $(document).ready(function() {
 //   $("form#pingPongInput").submit(function(event){
 //     event.preventDefault();
@@ -36,33 +66,3 @@
 // else {
 //     $("#output").append("<li>" + index + "</li>");
 // }
-
-
-// Business Logic
-function pingPongFunct(pingPongBot){
-  var pingPongResults = [];
-  for (index = 1; index <= pingPongBot; index++) {
-    if ((index % 3 === 0) && (index % 5 === 0)) {
-        pingPongResults.push('<li>pingpong</li>');
-    }
-    else if (index % 3 === 0) {
-        pingPongResults.push("<li>ping</li>");
-    }
-    else if (index % 5 === 0) {
-        pingPongResults.push("<li>pong</li>");
-    }
-    else {
-        pingPongResults.push("<li>" + index + "</li>");
-    }
-  }return pingPongResults;
-};
-
-// User Interface Logic
-$(document).ready(function() {
-  $("form#pingPongInput").submit(function(event){
-    event.preventDefault();
-    var input = parseInt($("input#userInput").val());
-    var results = pingPongFunct(input);
-    $("#output").empty().append(results);
-  });
-});
