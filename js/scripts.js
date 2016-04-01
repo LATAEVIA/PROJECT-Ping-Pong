3bdb542a3659cc446ff8314c52c1a8ea826bb7b0
LaTaevia <lataevia.berry@gmail.com>
2016-04-01 14:52:31 -0700

    update js to seperated business and user interface logic, add css styling and minor html edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,55 @@
+// $(document).ready(function() {
+//   $("form#pingPongInput").submit(function(event){
+//     event.preventDefault();
+//     var userInput = parseInt($("input#userInput").val());
+//     var bullets = "";
+//
+//     for (index = 1; index <= userInput; index++) {
+//       bullets += "<li>";
+//       if ((index % 3 === 0) && (index % 5 === 0)) {
+//           bullets += "ping-pong";
+//       }
+//       else if (index % 3 === 0) {
+//           bullets += "ping";
+//       }
+//       else if (index % 5 === 0) {
+//           bullets += "pong";
+//       }
+//       else {
+//           bullets += index;
+//       }
+//       bullets += "</li>";
+//       $("#output").empty().append(bullets);
+//     }
+//   });
+// });
+
+
+// Business Logic
+var pingPongFunction = function(userInput){
+  for (index = 1; index <= userInput; index++) {
+    if ((index % 3 === 0) && (index % 5 === 0)) {
+        $("#output").append("<li>pingpong</li>");
+    }
+    else if (index % 3 === 0) {
+        $("#output").append("<li>ping</li>");
+    }
+    else if (index % 5 === 0) {
+        $("#output").append("<li>pong</li>");
+    }
+    else {
+        $("#output").append("<li>" + index + "</li>");
+    }
+  }
+};
+
+// User Interface Logic
 $(document).ready(function() {
   $("form#pingPongInput").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("input#userInput").val());
-    var listItems = "";
-
-    for (index = 1; index <= userInput; index++) {
-      listItems += "<li>";
-      if ((index % 3 === 0) && (index % 5 === 0)) {
-          listItems += "ping-pong";
-      }
-      else if (index % 3 === 0) {
-          listItems += "ping";
-      }
-      else if (index % 5 === 0) {
-          listItems += "pong";
-      }
-      else {
-          listItems += index;
-      }
-      listItems += "</li>";
-      $("#output").empty().append(listItems);
-    }
+    pingPongFunction(userInput)
+      // $("#output").empty().append(result);
+      // var pong = $("#output").empty().append("pong");
   });
 });
